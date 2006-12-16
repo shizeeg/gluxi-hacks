27733f49b0efda2fdc49f23ae7781da07f57f40e
Dmitry Nezhevenko <dion@inhex.net>
2006-12-16 18:07:23 +0000

    [project @ 55] Admin plguin: AsyncList and AsyncCount commands

--- src/plugins/admin/adminplugin.cpp
+++ src/plugins/admin/adminplugin.cpp
@@ -1,12 +1,15 @@
 #include "adminplugin.h"
 #include "base/gluxibot.h"
+#include "base/asyncrequestlist.h"
+#include "base/asyncrequest.h"
 
 #include <gloox/client.h>
+#include <gloox/stanza.h>
 
 AdminPlugin::AdminPlugin(GluxiBot *parent)
 		: BasePlugin(parent)
 {
-	commands << "QUIT";
+	commands << "QUIT" << "TMPOWNERS" << "ASYNCCOUNT" << "ASYNCLIST";
 }
 
 
@@ -41,6 +44,43 @@ bool AdminPlugin::parseMessage(gloox::Stanza* s)
 		{
 			reply(s,"Only owner can do this");
 		}
+		return true;
+	}
+	
+	if (cmd=="ASYNCCOUNT")
+	{
+		reply(s,QString("Async requests count: %1").arg(bot()->asyncRequests()->count()));
+		return true;
+	}
+
+	if (cmd=="ASYNCLIST")
+	{
+		if (isFromOwner(s))
+		{
+			int cnt=bot()->asyncRequests()->count();
+			if (cnt==0)
+				reply(s,"No async requests found");
+			else
+			{
+				QString res;
+				for (int i=0; i<cnt; i++)
+				{
+					AsyncRequest *req=bot()->asyncRequests()->at(i);
+					QString plugin;
+					QString stanza;
+					if (req->plugin())
+						plugin=req->plugin()->name();
+					else
+						plugin="UNKNOWN";
+					if (req->stanza())
+						stanza=QString::fromStdString(req->stanza()->body());
+					res+=QString("\n%1: %2").arg(plugin).arg(stanza);
+				}
+				reply(s,"Active async requests: "+res);
+			}
+		}
+		else
+			reply(s,"Only owner can do this");
 		return true;
 	}
 
