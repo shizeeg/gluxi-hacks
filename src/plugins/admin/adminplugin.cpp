0b110a4efb3234a5338f9c72a34eb39cc3d3c0a9
Dmitry Nezhevenko <dion@inhex.net>
2008-05-18 20:22:45 +0300

    !admin sql command: execute sql query

--- src/plugins/admin/adminplugin.cpp
+++ src/plugins/admin/adminplugin.cpp
@@ -5,18 +5,27 @@
 #include "base/rolelist.h"
 #include "base/messageparser.h"
 #include "base/glooxwrapper.h"
+#include "base/datastorage.h"
 
 #include <gloox/client.h>
 #include <gloox/stanza.h>
 
 #include <QtDebug>
+#include <QSqlError>
+#include <QSqlRecord>
+#include <QVariant>
+
+QString restrictedTerms[]={"UPDATE", "TRUNCATE", "DROP", "CREATE", 
+			"GRANT", "ALTER", "INSERT", ""};
+
+const int TAB_SIZE=1;
 
 AdminPlugin::AdminPlugin(GluxiBot *parent)
 		:
 	BasePlugin(parent)
 {
 	commands << "QUIT" << "ROLES" << "ASYNCCOUNT" << "ASYNCLIST" << "PRESENCE"
-			<< "PRESENCEJID" << "SUBSCRIBE";
+			<< "PRESENCEJID" << "SUBSCRIBE" << "SQL";
 }
 
 AdminPlugin::~AdminPlugin()
@@ -170,6 +179,83 @@ bool AdminPlugin::parseMessage(gloox::Stanza* s)
 			bot()->client()->setPresence(target, gloox::PresenceChat, QString::null);
 			reply(s, "Request sent");
 		}
+		return true;
+	}
+	
+	if (cmd=="SQL")
+	{
+		QString query=parser.joinBody();
+		QString queryUp=query.toUpper();
+		int idx=0;
+		while (!restrictedTerms[idx].isEmpty())
+		{
+			int ps=queryUp.indexOf(restrictedTerms[idx]+" ");
+			if (ps>=0)
+			{
+				reply(s, QString("Restricted term found: %1 (at %2)")
+						.arg(restrictedTerms[idx]).arg(ps));
+				return true;
+			}
+			++idx;
+		}
+		
+		QSqlQuery sql=DataStorage::instance()->prepareQuery(query);
+		if (!sql.exec())
+		{
+			reply(s,sql.lastError().text());
+			return true;
+		}
+		QSqlRecord rec = sql.record();
+		
+		int cnt=rec.count();
+		QVector<QVector<QString> > resultTable;
+		
+		QVector<QString> line;
+		line.reserve(cnt);
+		for (int i=0; i<cnt; ++i)
+		{
+			line.append(rec.fieldName(i));
+		}
+		
+		resultTable.append(line);
+		line.clear();
+		
+		int totalRows=0;
+		while (sql.next())
+		{
+			line.clear();
+			for (int i=0; i<cnt; ++i)
+				line.append(sql.value(i).toString());
+			resultTable.append(line);
+			if (++totalRows==10)
+				break;
+		}
+		
+		QVector<int> colWidth(cnt);
+		colWidth.fill(0);
+		
+		for (int row=0; row<resultTable.count(); ++row)
+			for (int col=0; col<cnt; ++col)
+				if (resultTable[row][col].length()>colWidth[col])
+					colWidth[col]=resultTable[row][col].length();
+				
+		QString result;
+		for (int row=0; row<resultTable.count(); ++row)
+		{
+			QString line;
+			QVector<QString> rowVector=resultTable[row];
+			for (int col=0; col<cnt; ++col)
+			{
+				QString term=rowVector[col];
+				int l=term.length();
+				int maxL=colWidth[col];
+				line+=term;
+				line+=QString().fill(' ',(maxL-l)/TAB_SIZE + 1);
+			}
+			result+="\n"+line;
+		}		
+		
+		reply(s,QString("Result:")+result);
 		return true;
 	}
 
