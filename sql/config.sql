0d526aaee9f6456dbe258a48dcc9066d844be04e
Dmitry Nezhevenko <dion@inhex.net>
2008-06-01 21:43:03 +0300

    Move SQL scripts to sql/ dir. add config SQL script

---
+++ sql/config.sql
@@ -0,0 +1,3 @@
+insert into configuration_fields (plugin, name, field_type, description, default_value) VALUES (1, 'alists_every_presence', 2, 'Apply ALists to every presence', true);
+
+insert into configuration_fields (plugin, name, field_type, description, default_value) VALUES (1, 'alists_members', 2, 'Apply ALists to members', false);
