613d1fa2f6ee757f84dc5503b3052aea53251648
Sidgyck <sidgyck@gmail.com>
2010-08-20 00:55:00 +0400

    [FIX]: "!net translate gtr" not translate text after period (.) now fixed

--- src/plugins/net/translaterequest.cpp
+++ src/plugins/net/translaterequest.cpp
@@ -112,6 +112,12 @@ void TranslateRequest::exec()
 			deleteLater();
 			return;
 		}
+		// I don't know nothing about Yandex's new API
+		plugin()->reply(stanza(), "Yandex translate temporary disabled!");
+		deleteLater();
+		return;
+		//
+/*
 		QString url = QString("http://lingvo.yandex.ru/%1?text=%3&lang=%1&search_type=%2&st_translate=on")
 			.arg(myFrom).arg("lingvo").arg(myText);
 
@@ -122,6 +128,7 @@ void TranslateRequest::exec()
 		connect(http,SIGNAL(requestFinished(int, bool)), this, SLOT(httpRequestYandexFinished(int, bool)));
 		http->get(qurl.toEncoded());
 
+*/
 	}
 	else
 	{
@@ -129,64 +136,72 @@ void TranslateRequest::exec()
 		deleteLater();
 		return;
 	}
-
 }
 
 void TranslateRequest::httpRequestFinished(int, bool err)
 {
-	if (err || http->lastResponse().statusCode()!=200)
+	if (err || http->lastResponse().statusCode() != 200)
 	{
-		plugin()->reply(stanza(),"Failed to translate: "+http->lastResponse().reasonPhrase());
+		plugin()->reply(stanza(),
+		  "Failed to translate: "+http->lastResponse().reasonPhrase());
 		deleteLater();
 		return;
 	}
 	QString buf = http->readAll();
+	QRegExp exp("onmouseout=\"this.style.backgroundColor='#fff'\">(.*)</span>");
+	exp.setMinimal(true);
 
-	plugin()->reply(stanza(), getValue(buf,
-            "onmouseout=\"this.style.backgroundColor='#fff'\">(.*)</span>").trimmed() );
+	QString res;
+	int pos = 0;
+	while ((pos = exp.indexIn(buf, pos)) != -1)
+	{
+		res += removeHtml(exp.cap(1));
+		pos += exp.matchedLength();
+	}
+	plugin()->reply(stanza(), res.trimmed());
 	deleteLater();
 }
 
 void TranslateRequest::httpRequestListFinished(int, bool err)
 {
-  ///FIXIT: implement fetch list from the web.
+  ///FIXME: implement fetch list from the web.
 
-	QStringList dlist;
-	dlist		  << "auto	Detect language"
-			  << "ar	Arabic"
-			  << "bg	Bulgarian"
-			  << "ca	Catalan"
-			  << "zh-CN	Chinese"
-			  << "hr	Croatian"
-			  << "cs	Czech"
-			  << "da	Danish"
-			  << "nl	Dutch"
-			  << "en	English"
-			  << "tl	Filipino"
-			  << "fi	Finnish"
-			  << "fr	French"
-			  << "de	German"
-			  << "el	Greek"
-			  << "iw	Hebrew"
-			  << "hi	Hindi"
-			  << "id	Indonesian"
-			  << "it	Italian"
-			  << "ja	Japanese"
-			  << "ko	Korean"
-			  << "lv	Latvian"
-			  << "lt 	Lithuanian"
-			  << "no 	Norwegian"
-			  << "pl	Polish"
-			  << "pt 	Portuguese"
-			  << "ro	Romanian"
-			  << "ru	Russian"
-			  << "sr	Serbian"
-			  << "sk	Slovak"
-			  << "sl	Slovenian"
-			  << "es 	Spanish"
-			  << "sv	Swedish"
-			  << "uk	Ukrainian"
-			  << "vi	Vietnamese";
+	QStringList dlist; dlist
+        << "auto  Detect language"
+	<< "ar	Arabic"
+	<< "bg	Bulgarian"
+	<< "ca	Catalan"
+	<< "zh-CN	Chinese"
+	<< "hr	Croatian"
+	<< "cs	Czech"
+	<< "da	Danish"
+	<< "nl	Dutch"
+	<< "en	English"
+	<< "tl	Filipino"
+	<< "fi	Finnish"
+	<< "fr	French"
+	<< "de	German"
+	<< "el	Greek"
+	<< "iw	Hebrew"
+	<< "hi	Hindi"
+	<< "id	Indonesian"
+	<< "it	Italian"
+	<< "ja	Japanese"
+	<< "ko	Korean"
+	<< "lv	Latvian"
+	<< "lt 	Lithuanian"
+	<< "no 	Norwegian"
+	<< "pl	Polish"
+	<< "pt 	Portuguese"
+	<< "ro	Romanian"
+	<< "ru	Russian"
+	<< "sr	Serbian"
+	<< "sk	Slovak"
+	<< "sl	Slovenian"
+	<< "es 	Spanish"
+	<< "sv	Swedish"
+	<< "uk	Ukrainian"
+	<< "vi	Vietnamese";
 
 	plugin()->reply(stanza(), "Possible directions: \n" + dlist.join("\n"));
 	deleteLater();
@@ -195,14 +210,31 @@ void TranslateRequest::httpRequestListFinished(int, bool err)
 
 void TranslateRequest::httpRequestYandexFinished(int, bool err)
 {
+  /*
 	if (err || http->lastResponse().statusCode()!=200)
 	{
 		plugin()->reply(stanza(),"Failed to translate: "+http->lastResponse().reasonPhrase());
 		deleteLater();
 		return;
 	}
+  */
+	QHttpResponseHeader lastResponse = http->lastResponse();
+	if (err || lastResponse.statusCode()!=200 ) {
+		if ( lastResponse.statusCode()==301 || lastResponse.isValid() ) {
+			http->close();
+			QUrl qurl(lastResponse.value("location"));
+			http->setHost(qurl.host());
+			http->get(qurl.toEncoded());
+		} else {
+			plugin()->reply(stanza(), QString("Failed to translate: %1")
+					.arg(http->lastResponse().reasonPhrase()));
+			deleteLater();
+		}
+		return;
+	}
+
 	QString buf = http->readAll();
-	QString body = getValue(buf,"</span><div>(.*)</div></li>");
+	QString body = buf;//removeHtml(getValue(buf,"</table><div class=\"b-translate__value\">(.*)</div></div>"));
 
 	plugin()->reply(stanza(), (body.isEmpty()) ? "<not found>":body );
 	deleteLater();
@@ -210,14 +242,14 @@ void TranslateRequest::httpRequestYandexFinished(int, bool err)
 
 void TranslateRequest::httpRequestListYandexFinished(int, bool err)
 {
-	QStringList dlist;
-		dlist << "en	Englist	<-> Russian";
-		dlist << "de	Deutch	<-> Russian";
-		dlist << "fr	French	<-> Russian";
-		dlist << "it	Italian	<-> Russian";
-		dlist << "es	Spanish	<-> Russian";
-		dlist << "uk	Ukraine	<-> Russian";
-		dlist << "la	Latin	<-> Russian";
+	QStringList dlist; dlist
+	<< "en	Englist	<-> Russian"
+	<< "de	Deutch	<-> Russian"
+	<< "fr	French	<-> Russian"
+	<< "it	Italian	<-> Russian"
+	<< "es	Spanish	<-> Russian"
+	<< "uk	Ukraine	<-> Russian"
+	<< "la	Latin	<-> Russian";
 
 	plugin()->reply(stanza(), "Possible directions: \n" + dlist.join("\n"));
 	deleteLater();
